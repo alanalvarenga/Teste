360ec08a6bd98c64220b3663fadf56d7dc60b9d2
Pedro Filho <pedroapfilho20@hotmail.com>
2017-01-08 18:18:18 -0200

    Tirando o bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import { Provider } from 'react-redux';
 //import { createStore, applyMiddleware } from 'redux';
-import { Grid, Row, Col, Input, Button, Collapse } from 'react-bootstrap';
+//import { Grid, Row, Col, Input, Button, Collapse } from 'react-bootstrap';
 //import Wavesurfer from './react-wavesurfer';
 import MyComponent from './mycomponent';
 
