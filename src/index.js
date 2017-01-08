0bb21137b8ce5ac4d59ff9bcef4c44fb0c21d356
alanalvarenga <alanc.alvarenga@gmail.com>
2017-01-07 22:08:51 -0200

    Primeiro commit
    versão html v.0.1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { Provider } from 'react-redux';
+//import { createStore, applyMiddleware } from 'redux';
+import { Grid, Row, Col, Input, Button, Collapse } from 'react-bootstrap';
+//import Wavesurfer from 'react-wavesurfer';
+import MyComponent from './mycomponent';
+
+
+//import App from './components/app';
+//import reducers from './reducers';
+
+//const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+ReactDOM.render(React.createElement(MyComponent, null)
+  , document.querySelector('.container'));
